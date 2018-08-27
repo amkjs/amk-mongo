9f9546cfe66437dba5f9a4f043119adddc200685
clouds8 <clouds8@sina.cn>
2018-08-27 16:15:50 +0800

    update mongodb dependencies.

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,32 +1,41 @@
 module.exports = {
     extends: 'eslint:recommended',
     parserOptions: {
-        ecmaVersion: 2017,
-		sourceType: "module",
-        ecmaFeatures: {
-            jsx: true
-        }
+        ecmaVersion: 8,
+        sourceType: 'module'
     },
     rules: {
         'global-require': 2,
-		'default-case': 2,
-		eqeqeq: [2, 'smart'],
-		'no-eq-null': 2,
-		strict: [2, 'global'],
-		'callback-return': 2,
-		'no-process-env': 2,
-		'no-process-exit': 2,
-		'no-var': 2
+        'default-case': 2,
+        eqeqeq: [2, 'smart'],
+        'no-eq-null': 2,
+        strict: [2, 'global'],
+        'callback-return': 2,
+        'no-process-exit': 2,
+        'no-var': 2,
+        'no-unused-expressions': 1,
+        semi: 2,
+        'func-names': 1,
+        'prefer-destructuring': 2,
+        'object-shorthand': 2,
+        quotes: ['error', 'single'],
+        'no-buffer-constructor': 2,
+        'max-len': [
+            1,
+            {
+                code: 100,
+            }
+        ]
     },
     globals: {
         require: false,
         console: false,
         module: false,
-        '__dirname': false,
-        Promise: false
+        __dirname: false
     },
     env: {
         node: true,
-		mocha: true
+        mocha: true,
+        es6: true
     }
-}
+};
